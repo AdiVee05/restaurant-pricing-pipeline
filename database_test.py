c3187cb76bf54a26532f49d75b8ad60cd377fff9
Aditya Veeramony <aditya.dfw@gmail.com>
2026-06-13 16:15:31 -0500

    Made a connection through SQLalchemy and got a database going.

--- database_test.py
+++ database_test.py
@@ -1,2 +1,6 @@
 from sqlalchemy import create_engine
-engine = create_engine("sqlite://", echo=True)
+# engine = create_engine("sqlite://", echo=True) 
+# only for a random SQL Lite Server
+engine = create_engine("mssql+pyodbc://adityav:########@adityavserver.database.windows.net/free-sql-db-6841086?driver=ODBC+Driver+18+for+SQL+Server")
+with engine.connect() as conn:
+    print("Connection!")
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-
 app = FastAPI()
-
-
 @app.get("/")
 async def root():
     return {"message": "Testing API code"}
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import os
+
+load_dotenv()
+engine = create_engine(os.getenv("DATABASE_URL"))
+with engine.connect() as conn:
+    print("Connection!")
+
+# engine = create_engine("sqlite://", echo=True) 
+# only for a random SQL Lite Server
